c4ee682d3abe322060d3d9b97810b60d330cfc93
David Jiang <djiang86@binghamton.edu>
2018-08-28 22:54:29 +0000

    different Big O

--- Big-O/Big-O-n.js
+++ Big-O/Big-O-n.js
@@ -7,7 +7,13 @@ function countUpAndDown(n) {
         console.log(j);
     }
 }
-
 countUpAndDown(5);
-
 // this is O(5n) which simplifies to O(n)
+
+function logAtLeast5(n) {
+    for (let i = 1; i <= Math.max(5, n); i++) {
+        console.log(i);
+    }
+}
+
+logAtLeast5(10);
