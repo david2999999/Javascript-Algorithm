891812e4733d3fa7a50bcf482ad8a8ff8a7e212e
David Jiang <djiang86@binghamton.edu>
2018-08-28 23:41:04 +0000

    function analysis for numbers

---
+++ Big-O/Objects.js
@@ -0,0 +1,10 @@
+// insertion = O(1)
+// Removal = O(1)
+// searching = O(n)
+// Access = O(1)
+
+let instructor = {
+    firstName: 'kelly',
+    isInstructor: true,
+    favoriteNumbers: [1,2,3,4]
+}
