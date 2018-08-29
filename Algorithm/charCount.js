e29338820a663ae4c88baa5a65dd8b816f1ac584
David Jiang <djiang86@binghamton.edu>
2018-08-29 01:21:39 +0000

    find char count in string

---
+++ Algorithm/charCount.js
@@ -0,0 +1,16 @@
+function charCount(str) {
+    let result = {};
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase();
+        if (result[char] > 0) {
+            result[char]++;
+        } else {
+            result[char] = 1;
+        }
+    }
+
+    return result;
+}
+
+console.log(charCount('hello'));
+console.log(charCount('Hi there!'));
