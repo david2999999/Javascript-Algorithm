9196b3f274cd0caca6bd901bb4716ef7031b4b30
David Jiang <djiang86@binghamton.edu>
2018-08-29 01:41:09 +0000

    refactored findChar

--- Algorithm/charCount.js
+++ Algorithm/charCount.js
@@ -12,5 +12,17 @@ function charCount(str) {
     return result;
 }
 
+function charCountRefactored(str) {
+    let obj = {};
+    for (let char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] = ++obj[char] || 1;
+        }
+    }
+
+    return obj;
+}
+
 console.log(charCount('hello'));
 console.log(charCount('Hi there!'));
