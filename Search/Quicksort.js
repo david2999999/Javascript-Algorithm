e80b15dd56d24b59bc13e49df77f5df2f3307b6f
David Jiang <djiang86@binghamton.edu>
2018-10-22 18:30:13 +0000

    quick sort

--- Search/Quicksort.js
+++ Search/Quicksort.js
@@ -1,4 +1,16 @@
-function pivot(arr, start = 0, end = arr.length + 1) {
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+
+    return arr;
+}
+
+console.log(quickSort([1,1,5,1,2,5,12,2,1,23]));
+
+function pivot(arr, start = 0, end = arr.length - 1) {
     function swap(arr, idx1, idx2) {
         let temp = arr[idx1];
         arr[idx1] = arr[idx2];
