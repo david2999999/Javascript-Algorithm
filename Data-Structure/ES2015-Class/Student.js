264f815e7523ca1946dac2781c1ab2f116f46da6
David Jiang <djiang86@binghamton.edu>
2018-10-24 03:40:55 +0000

    instance methods

--- Data-Structure/ES2015-Class/Student.js
+++ Data-Structure/ES2015-Class/Student.js
@@ -3,7 +3,14 @@ class Student {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    fullName() {
+        console.log(`Your full name is ${this.firstName} ${this.lastName}`);
+    }
 }
 
 let firstStudent = new Student("Dav", "Qwe");
 let secondStudent = new Student("Harry", "Steele");
+
+firstStudent.fullName();
+secondStudent.fullName();
