9ba8d55b31be1c2f1f6bcb1fcc12633af33bd79e
David Jiang <djiang86@binghamton.edu>
2018-10-24 13:19:11 +0000

    pushing a val into linked list

--- Data-Structure/Singly-LinkedList/SinglyLinkedList.js
+++ Data-Structure/Singly-LinkedList/SinglyLinkedList.js
@@ -6,8 +6,28 @@ class SinglyLinkedList{
     }
 
     push(val) {
-        
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+}
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
     }
 }
 
 let list = new SinglyLinkedList();
+list.push("Hi");
+list.push("hello");
+console.log(list);
