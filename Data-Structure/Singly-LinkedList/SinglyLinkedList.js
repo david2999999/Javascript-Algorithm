99ad40ccefaa03288fa6fd0a24e031ec517a0891
David Jiang <djiang86@binghamton.edu>
2018-10-24 14:05:31 +0000

    shift a value in a linkedlist

--- Data-Structure/Singly-LinkedList/SinglyLinkedList.js
+++ Data-Structure/Singly-LinkedList/SinglyLinkedList.js
@@ -41,6 +41,20 @@ class SinglyLinkedList{
         return current;
     }
 
+    shift() {
+        if (this.length === 0) return undefined;
+
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+
+        if (this.length === 0) {
+            this.tail = null;
+        }
+
+        return currentHead;
+    }
+
 
     traverse() {
         let current = this.head;
