2cb5e8384f7558e7af3234e9dd27af4ed5e21910
David Jiang <djiang86@binghamton.edu>
2018-10-24 04:40:39 +0000

    started singly linked list

---
+++ Data-Structure/Singly-LinkedList/SinglyLinkedList.js
@@ -0,0 +1,13 @@
+class SinglyLinkedList{
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    push(val) {
+        
+    }
+}
+
+let list = new SinglyLinkedList();
