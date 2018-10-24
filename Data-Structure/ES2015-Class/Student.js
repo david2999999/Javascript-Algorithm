8711b0c3805abad470d6094da1ec64e915deae32
David Jiang <djiang86@binghamton.edu>
2018-10-24 03:38:27 +0000

    ES2015 class review

---
+++ Data-Structure/ES2015-Class/Student.js
@@ -0,0 +1,9 @@
+class Student {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+let firstStudent = new Student("Dav", "Qwe");
+let secondStudent = new Student("Harry", "Steele");
