4a8bd4e58c1dabadbd83ee8aa90ccac9ee53beb0
David Jiang <djiang86@binghamton.edu>
2018-10-26 13:56:46 +0000

    started doubly linkedlist

---
+++ Data-Structure/Doubly-LinkedList/DoublyLinkedList.js
@@ -0,0 +1,15 @@
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
