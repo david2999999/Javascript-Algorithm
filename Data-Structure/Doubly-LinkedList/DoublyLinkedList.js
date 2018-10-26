8d158db86e62ec5c55553474ac9b13136b6feee5
David Jiang <djiang86@binghamton.edu>
2018-10-26 14:24:49 +0000

    pushing a value into doubly linkedlist

--- Data-Structure/Doubly-LinkedList/DoublyLinkedList.js
+++ Data-Structure/Doubly-LinkedList/DoublyLinkedList.js
@@ -4,6 +4,21 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
 }
 
 class Node {
