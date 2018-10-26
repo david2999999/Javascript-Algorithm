92fb74e241850870fe78fc6b43836a563e7907c1
David Jiang <djiang86@binghamton.edu>
2018-10-26 17:11:34 +0000

    set value at specific index

--- Data-Structure/Doubly-LinkedList/DoublyLinkedList.js
+++ Data-Structure/Doubly-LinkedList/DoublyLinkedList.js
@@ -84,8 +84,18 @@ class DoublyLinkedList {
                 count--;
             }
         }
-        
+
         return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+
+        return false;
     }
 }
 
