1075683fc536e0f4c384179ae86830a66931d1e7
David Jiang <djiang86@binghamton.edu>
2018-10-31 02:43:57 +0000

    inserting node into tree iteratively

--- Data-Structure/Tree/BST.js
+++ Data-Structure/Tree/BST.js
@@ -10,7 +10,44 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(val) {
+        let newNode = new Node(val);
+
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (val === current.value) return undefined;
+                if (val < current.value) {
+                    if (!current.left) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.value) {
+                    if (!current.right) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
+console.log(tree);
+
