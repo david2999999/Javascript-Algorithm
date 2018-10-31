27d33bf990eed012ce7be169d078694e5a1ea709
David Jiang <djiang86@binghamton.edu>
2018-10-31 04:19:19 +0000

    depth first preorder

--- Data-Structure/Tree/BST.js
+++ Data-Structure/Tree/BST.js
@@ -74,6 +74,19 @@ class BinarySearchTree {
 
         return data;
     }
+
+    DFSPreOrder() {
+        let data = [];
+
+        function traverse(node) {
+            data.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
 }
 
 let tree = new BinarySearchTree();
@@ -84,4 +97,5 @@ tree.insert(3);
 tree.insert(8);
 tree.insert(20);
 console.log(tree.BSF());
+console.log(tree.DFSPreOrder());
 
