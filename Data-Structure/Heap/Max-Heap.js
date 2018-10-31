ad3beb80d758777e5671b2003aad4b241b4e6a77
David Jiang <djiang86@binghamton.edu>
2018-10-31 15:37:23 +0000

    extract max from heap

--- Data-Structure/Heap/Max-Heap.js
+++ Data-Structure/Heap/Max-Heap.js
@@ -22,6 +22,50 @@ class MaxBinaryHeap {
             idx = parentIdx;
         }
     }
+
+    extractMax() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+
+            // trickle down
+            this.sinkDown();
+        }
+
+        return max;
+    }
+
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if ((!swap && rightChild > element) || (swap && rightChild > leftChild)) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if (!swap) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap();
@@ -34,4 +78,6 @@ heap.insert(12);
 heap.insert(33);
 heap.insert(1);
 heap.insert(100);
+heap.extractMax();
+heap.extractMax();
 console.log(heap);
