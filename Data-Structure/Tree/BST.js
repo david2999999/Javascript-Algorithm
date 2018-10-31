b3e547bf2b4c12989e8092ed31338927041d9de5
David Jiang <djiang86@binghamton.edu>
2018-10-31 04:33:34 +0000

    DFS post order

--- Data-Structure/Tree/BST.js
+++ Data-Structure/Tree/BST.js
@@ -87,6 +87,19 @@ class BinarySearchTree {
         traverse(this.root);
         return data;
     }
+
+    DFSPostOrder() {
+        let data = [];
+
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+        return data;
+    }
 }
 
 let tree = new BinarySearchTree();
