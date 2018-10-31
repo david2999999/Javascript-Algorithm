72f4e0371e946341119f11bc7b20f92f499236a1
David Jiang <djiang86@binghamton.edu>
2018-10-31 02:28:13 +0000

    started BST

---
+++ Data-Structure/Tree/BST.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
