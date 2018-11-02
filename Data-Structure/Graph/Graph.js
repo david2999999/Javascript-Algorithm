6fcff75c311e738085cd65d108fa27f95b99fbd4
David Jiang <djiang86@binghamton.edu>
2018-11-02 17:48:10 +0000

    adding edge to graph

--- Data-Structure/Graph/Graph.js
+++ Data-Structure/Graph/Graph.js
@@ -6,8 +6,17 @@ class Graph {
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+
+    addEdge(v1, v2) {
+        if (!this.adjacencyList[v1]) this.addVertex(v1);
+        if (!this.adjacencyList[v2]) this.addVertex(v2);
+
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
 }
 
 let g = new Graph();
 g.addVertex("Tokyo");
+g.addEdge("Tokyo", "San Francisco");
 console.log(g);
