9c800a37aef2bf0de8af5def8aff0df1debff1ea
david2999999 <djiang86@binghamton.edu>
2020-03-17 09:13:23 -0400

    creating arrays with square brackets

---
+++ Data Structures and Algorithms with Javascript PDF/Chapter 2 - Arrays/create-array.js
@@ -0,0 +1,10 @@
+// Simplest way to declare an array is using the [] operator
+    var numbers = [];
+
+// when we create an array in this manner, we will have an array with length of 0
+    // this can be checked with the built-in length property
+     console.log(numbers.length);
+
+// Another way to create an array is to declare an array variable with a set of elements inside the [] operator
+    var numbers2 = [1, 2, 3, 4, 5];
+    console.log(numbers2.length);
