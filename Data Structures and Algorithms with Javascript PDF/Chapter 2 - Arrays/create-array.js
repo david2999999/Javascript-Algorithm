21c6c70752bc1fbbd9356fa096cb393aca98162b
david2999999 <djiang86@binghamton.edu>
2020-03-17 09:23:29 -0400

    declaring arrays using the Array constructor

--- Data Structures and Algorithms with Javascript PDF/Chapter 2 - Arrays/create-array.js
+++ Data Structures and Algorithms with Javascript PDF/Chapter 2 - Arrays/create-array.js
@@ -1,10 +1,23 @@
 // Simplest way to declare an array is using the [] operator
-    var numbers = [];
+var numbers = [];
 
 // when we create an array in this manner, we will have an array with length of 0
     // this can be checked with the built-in length property
-     console.log(numbers.length);
+ console.log(numbers.length); // displays 0
 
 // Another way to create an array is to declare an array variable with a set of elements inside the [] operator
-    var numbers2 = [1, 2, 3, 4, 5];
-    console.log(numbers2.length);
+var numbers2 = [1, 2, 3, 4, 5];
+console.log(numbers2.length); // displays 5
+
+
+// you can also create array by calling the Array constructor
+var numbers3 = new Array();
+console.log(numbers3.length); // displays 0
+
+// you can also call the array constructor with a set of elements as arguments to the constructor
+var numbers4 = new Array(1, 2, 3, 4, 5);
+console.log(numbers4.length); // displays 5
+
+// Lastly, you can create an array by calling the Array constructor with a single argument specifying the length of the array
+var numbers5 = new Array(10);
+console.log(numbers5.length); // displays 10
