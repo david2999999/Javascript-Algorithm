0172cc2d1f3a510a30cf7222bf1ff2332f9c63dd
david2999999 <djiang86@binghamton.edu>
2020-03-18 09:03:17 -0400

    string representation of an array with join() and toString()

---
+++ Data Structures and Algorithms with Javascript PDF/Chapter 2 - Arrays/Accessor Functions/string-representation.js
@@ -0,0 +1,9 @@
+// There are two functions that returns the string representations of an array, 'join()' and 'toString()'
+    // Both functions return a string containing the elements of the array delimited by commas
+var names = ["David", "Cynthia", "Raymond", "Clayton", "Mike", "Jennifer"];
+
+var namestr = names.join();
+console.log(namestr); // David,Cynthia,Raymond,Clayton,Mike,Jennifer
+
+namestr = names.toString();
+console.log(namestr); // David,Cynthia,Raymond,Clayton,Mike,Jennifer
