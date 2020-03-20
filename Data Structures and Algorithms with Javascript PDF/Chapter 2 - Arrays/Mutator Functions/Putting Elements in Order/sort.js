f8766f74d5322a3b3b4288a88a7128f8b7387289
david2999999 <djiang86@binghamton.edu>
2020-03-20 09:47:13 -0400

    passing in an ordering function to sort() function to sort numbers in ascending order

--- Data Structures and Algorithms with Javascript PDF/Chapter 2 - Arrays/Mutator Functions/Putting Elements in Order/sort.js
+++ Data Structures and Algorithms with Javascript PDF/Chapter 2 - Arrays/Mutator Functions/Putting Elements in Order/sort.js
@@ -8,3 +8,20 @@ console.log(names); // Bryan,Clayton,Cynthia,David,Mike,Raymond
 var nums = [3,1,2,100,4,200];
 nums.sort();
 console.log(nums); // 1,100,2,200,3,4
+
+// The sort() function sorts data lexicographically, assuming the data elements are strings
+    // In the previous example, the elements are numbers, so it is not sorted numerically
+// We can make the sort() function work correctly for numbers by passing in an ordering function as the first
+// argument to the function, which sort() will use to sort the array elements.
+    // The function allows the sort() function to compare pairs of array elements to determine their correct order
+
+// For numbers, the ordering function can simply subtract one number from another number
+    // if the returned is negative, the left operand is less than the right operand
+    // if the returned is positive, the left operand is bigger than the right operand
+    // if the return is zero, the left operand is equal to the right operand
+function compare(num1, num2) {
+    return num1 - num2;
+}
+nums = [3,1,2,100,4,200];
+nums.sort(compare);
+console.log(nums); // 1,2,3,4,100,200
