1df877b93244d8439bcaa01c12425e73f441fc7b
David Jiang <djiang86@binghamton.edu>
2020-04-02 08:32:41 -0400

    radix sort with queues explaination

--- Data Structures and Algorithms with Javascript PDF/Chapter 5 - Queues/Applications/Square Dance/square-dance.js
+++ Data Structures and Algorithms with Javascript PDF/Chapter 5 - Queues/Applications/Square Dance/square-dance.js
@@ -40,3 +40,16 @@ function dance(males, females) {
     }
     console.log();
 }
+
+var maleDancers = new Queue();
+var femaleDancers = new Queue();
+getDancers(maleDancers, femaleDancers);
+dance(maleDancers, femaleDancers);
+
+if (!femaleDancers.empty()) {
+    console.log(femaleDancers.front().name + " is waiting to dance.");
+}
+
+if (!maleDancers.empty()) {
+    console.log(maleDancers.front().name + " is waiting to dance.");
+}
