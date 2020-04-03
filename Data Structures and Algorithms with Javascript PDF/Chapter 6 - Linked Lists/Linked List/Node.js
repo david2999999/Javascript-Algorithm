e66158f528e18eddc2079c656fa587dca47eb579
David Jiang <djiang86@binghamton.edu>
2020-04-03 09:23:35 -0400

    Node class for linked list

---
+++ Data Structures and Algorithms with Javascript PDF/Chapter 6 - Linked Lists/Linked List/Node.js
@@ -0,0 +1,8 @@
+function Node(element) {
+    this.element = element;
+    this.next = null;
+}
+
+// The Node class consists of two properties
+    // element - stores the node's data
+    // next - stores a link to the next node in the linked list
