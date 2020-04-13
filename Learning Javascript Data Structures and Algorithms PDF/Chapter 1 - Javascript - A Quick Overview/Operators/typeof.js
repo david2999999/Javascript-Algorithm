f4746cda8db38d95bf233bd83c4e7e5110b4b398
David Jiang <djiang86@binghamton.edu>
2020-04-13 11:28:02 -0400

    using the typeof operator

---
+++ Learning Javascript Data Structures and Algorithms PDF/Chapter 1 - Javascript - A Quick Overview/Operators/typeof.js
@@ -0,0 +1,10 @@
+console.log('typeof num:', typeof num); // number
+console.log('typeof Packt:', typeof 'Packt'); // string
+console.log('typeof true:', typeof true); // boolean
+console.log('typeof [1,2,3]:', typeof [1,2,3]); // object
+console.log('typeof {name:John}:', typeof {name:'John'}); // object
+
+// Note: the array has a type of object, arrays in JS is just an object with numeric keys,
+//       which are the indices of the array
+//       As a result, the implementation of array in JS is slower than the array in other programming languages
+
