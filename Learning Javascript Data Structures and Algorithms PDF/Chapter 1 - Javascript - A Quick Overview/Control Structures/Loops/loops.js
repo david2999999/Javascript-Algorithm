7cb4598f95d96935e40e379881f585ab5f4fd0bb
David Jiang <djiang86@binghamton.edu>
2020-04-15 08:57:35 -0400

    three types of loops, for/while/do while

---
+++ Learning Javascript Data Structures and Algorithms PDF/Chapter 1 - Javascript - A Quick Overview/Control Structures/Loops/loops.js
@@ -0,0 +1,19 @@
+// The 'for' loop is exactly the same as in C and Java
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// The 'while' loop is executed while the condition is true
+i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+// The 'do-while' loop is similar to the 'while' loop but the condition is
+// evaluated after executing the script
+i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
